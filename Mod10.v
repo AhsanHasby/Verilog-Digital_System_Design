b0d254b2fa009c7b454cd66cc76b8caf5419dfe0
Md Maskaowat Ahsan Hasby <mahahsanhasby@gmail.com>
2026-04-15 23:06:54 +0600

    MOD10 + Master JK

--- Mod10.v
+++ Mod10.v
@@ -1,37 +1,54 @@
-module Mod10 (
-	input clk,
-	output [3:0] count
-);
-	reg j = 1, k = 1;
-	wire [3:0] q, qb;
-	wire rst;
-	
-	nand (rst, q[3], q[1]);
+`timescale 1ns / 1ps
 
-	JK_FF ff0 (.j(j), .k(k), .clk(clk), .clr(rst), .q(q[0]), .qbar(qb[0])); 
-	JK_FF ff1 (.j(j), .k(k), .clk(q[0]), .clr(rst), .q(q[1]), .qbar(qb[1])); 
-	JK_FF ff2 (.j(j), .k(k), .clk(q[1]), .clr(rst), .q(q[2]), .qbar(qb[2])); 
-	JK_FF ff3 (.j(j), .k(k), .clk(q[2]), .clr(rst), .q(q[3]), .qbar(qb[3])); 
-	
-	assign count = {q[3], q[2], q[1], q[0]};
+module JKFF (J, K, Clock, clr_n, Q, Qn);
+    input J, K, Clock, clr_n;
+    output Q, Qn;
 
+    wire clk_inv;
+    wire j_gate, k_gate;
+    wire m_q, m_qn;
+    wire s_j_gate, s_k_gate;
+    not (clk_inv, Clock);
+
+    nand (j_gate, J, Clock, Qn, clr_n);
+    nand (k_gate, K, Clock, Q);
+    nand (m_q, j_gate, m_qn);
+    nand (m_qn, k_gate, m_q, clr_n);
+
+    nand (s_j_gate, m_q, clk_inv);
+    nand (s_k_gate, m_qn, clk_inv);
+    nand (Q, s_j_gate, Qn);
+    nand (Qn, s_k_gate, Q, clr_n);
 endmodule
 
-module Mod10_TB;
-	reg clk;
-	wire [3:0] count;
+module Mod10Counter (count, clk, external_reset);
+    output [3:0] count;
+    input clk, external_reset;
+    
+    wire [3:0] q_not;
+    wire rst10;
+    wire master_clr_n;
 
-	Mod10 mod (.clk(clk), .count(count));
-	// Clock Generation :
-	initial begin 
-		clk = 0;
-		forever #50 clk = ~clk;
-	end
-	// Simulation : 
-	initial begin 
-		$monitor("Count = %b (%0d)", count, count);
-		#2000;
-		$finish;
-	end
+    nand #1 (rst10, count[3], count[1]); 
+    and #1 (master_clr_n, external_reset, rst10);
 
+    JKFF ff0 (1'b1, 1'b1, clk, master_clr_n, count[0], q_not[0]);
+    JKFF ff1 (1'b1, 1'b1, count[0], master_clr_n, count[1], q_not[1]);
+    JKFF ff2 (1'b1, 1'b1, count[1], master_clr_n, count[2], q_not[2]);
+    JKFF ff3 (1'b1, 1'b1, count[2], master_clr_n, count[3], q_not[3]);
+endmodule
+
+module tb_Mod10Counter();
+    reg clk;
+    reg external_reset;
+    wire [3:0] count;    
+    Mod10Counter uut ( .count(count), .clk(clk), .external_reset(external_reset));    
+    always #5 clk = ~clk;
+    initial begin
+        clk = 0;
+        external_reset = 0;        
+        #15 external_reset = 1;        
+        #500;
+        $stop;
+    end
 endmodule
