51c9fdb143678e4489e823043d4c416db30744d7
Md Maskaowat Ahsan Hasby <mahahsanhasby@gmail.com>
2026-04-15 12:22:45 +0600

    Four bit Full Adder

--- FullAdder_4bit.v
+++ FullAdder_4bit.v
@@ -4,6 +4,7 @@ module FullAdder_1bit (
 );
 	assign sum = a ^ b ^ cin;
 	assign cout = (a & b) | (b & cin) | (a & cin);
+
 endmodule
 
 module FullAdder_4bit(
@@ -12,5 +13,13 @@ module FullAdder_4bit(
 	output [3:0] sum,
 	output cout
 );
-	
+	wire [3:0] carry;
+
+	FullAdder_1bit FA0(.a(a[0]), .b(b[0]), .cin(cin),       .sum(sum[0]), .cout(carry[0]));
+	FullAdder_1bit FA1(.a(a[1]), .b(b[1]), .cin(carry[0]),  .sum(sum[1]), .cout(carry[1]));
+	FullAdder_1bit FA2(.a(a[2]), .b(b[2]), .cin(carry[1]),  .sum(sum[2]), .cout(carry[2]));
+	FullAdder_1bit FA3(.a(a[3]), .b(b[3]), .cin(carry[2]),  .sum(sum[3]), .cout(carry[3]));
+
+	assign cout = carry[3];
+
 endmodule
