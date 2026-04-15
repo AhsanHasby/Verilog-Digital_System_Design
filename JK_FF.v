6acd0e35857df914cd41f92aa06b93156d297560
Md Maskaowat Ahsan Hasby <mahahsanhasby@gmail.com>
2026-04-15 22:19:08 +0600

    Basic JK ff

--- JK_FF.v
+++ JK_FF.v
@@ -1,28 +1,17 @@
-// Master Slave JK Flip-Flop:
-module JK_FF(
-	input j, k, clk, clr,
-	output q, qbar
+module JK_FF (
+    input j,
+    input k,
+    input clk,
+    input rstn,
+    output reg q,
+    output q_bar
 );
-	wire clk_bar, clr_bar;
-	wire m1, m2, s1, s2;
-	wire q_m, qbar_m, q_s, qbar_s;
-	
-	not (clk_bar, clk);
-	not (clr_bar, clr);
+    assign q_bar = ~q;
 
-	nand (m1, j, qbar_m, clk);
-	nand (m2, k, q_m, clk);
-	nand (q_m, m1, qbar_m);
-	nand (qbar_m, m2, q_m);
-
-	nand (s1, q_m, qbar_s, clk_bar);
-	nand (s2, qbar_m, q_s, clk_bar);
-	nand (q_s, s1, qbar_s);
-	nand (qbar_s, s2, q_s);
-
-	nand (q, q_s, clr_bar);
-	nand (qbar, qbar_s, clr_bar);
-		
-
+    always @(posedge clk or negedge rstn) begin
+        if (!rstn)
+		q <= 1'b0;
+        else
+		q<=~q; 
+    end
 endmodule
-
